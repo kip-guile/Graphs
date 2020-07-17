55d6309794aa78c75099aff6b7e9da3523dbe846
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-07-17 08:10:27 +0100

    mvp

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,7 +1,7 @@
 from room import Room
 from player import Player
 from world import World
-from util import Stack
+from util import Stack, Queue
 
 import random
 from ast import literal_eval
@@ -31,36 +31,53 @@ player = Player(world.starting_room)
 traversal_path = []
 stack = Stack()
 graph = {}
-
-while len(graph) < 500 and len(traversal_path) < 2000:
-    print(traversal_path)
+# and len(traversal_path) < 2000
+# while graph has not been traversed and all
+while len(graph) < 500:
+    print(stack.stack)
+    # build graph
+    # set current room to be room player is in
     current_room = player.current_room.id
 
+    # find room exits
     if current_room not in graph:
+        # if current room is not in graph, then set current exits to {}
         current_exits = {}
 
+        # loop through exits array, creating a key in current exits dict with value '?'
         for exit in player.current_room.get_exits():
             current_exits[exit] = '?'
+        # initial graph in the form 0: {'n': '?', 's': '?', 'w': '?', 'e': '?'}
         graph[current_room] = current_exits
 
+    # get object with available exits
     current_exits = graph[current_room]
 
+    # move north if possible
+    # check if the room north exists and has never been visited
     if 'n' in current_exits and current_exits['n'] == '?':
+        # if so, move north
         player.travel('n')
+        # append n to traversal array
         traversal_path.append('n')
+        # next room is the id of current room we are in because we moved
         next_room = player.current_room.id
+        # change value of current exits[n] from ? to room id to show it has been visited
         current_exits['n'] = next_room
 
+        # add next room to graph
         if next_room not in graph:
             next_room_exits = {}
-
+            # loop through exits array, creating a key in current exits dict with value '?'
             for exit in player.current_room.get_exits():
                 next_room_exits[exit] = '?'
-
-            next_room_exits['s'] = current_room
+            # change value of current exits[s] from ? to room id to show it has been visited
+            next_room_exits['s'] = next_room
+            # add room to graph
             graph[next_room] = next_room_exits
         else:
-            graph[next_room]['s'] = current_room
+            graph[next_room]['s'] = next_room
+        # push opposite direction into stack
         stack.push('s')
 
     elif 'e' in current_exits and current_exits['e'] == '?':
@@ -116,13 +133,16 @@ while len(graph) < 500 and len(traversal_path) < 2000:
         else:
             graph[next_room]['e'] = current_room
         stack.push('e')
-
+    # go back if there is no direction to move in
     else:
+        # the opposite direction is in stack so pop to get it
         goBack = stack.pop()
+        # break is stack is empty
         if goBack is None:
             break
-
+        # move in popped direction to go back
         player.travel(goBack)
+        # add popped direction to traversal path
         traversal_path.append(goBack)
 
 # TRAVERSAL TEST
